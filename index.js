825cf58ecc86865722b23a1070ac1b8dc88ff421
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-05 17:17:24 -0800

    commit tree d7b65b278cbf050962bff42b7bd0a49dc37d3ff5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+const employee = {
+    name: {
+        firstName: "Kittah",
+        lastName: "Deslauriers",
+    },
+    address: {
+        street: {
+            line1: "11 Broadway",
+            line2: "PH",
+        },
+        city: "San Francisco",
+        state: "CA",
+        zipCode: 94109,
+    }
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copyEmployee = {...employee};
+    delete copyEmployee[key];
+    return copyEmployee;
+    
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
